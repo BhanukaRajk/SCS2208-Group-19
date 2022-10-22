36dde01aa82f0416ef0500b968ddcb4a3b57bfac
kavishka-sulakshana <kss20001218@gmail.com>
2022-10-23 02:41:49 +0530

    do read mechanic data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import Sample from './components/Sample';
+import MechanicDash from './components/admin/MechanicDash';
 
 function App() {
   return (
     <div className="App">
-     <Sample color="red"/>
-     <Sample color="blue"/>
-     <Sample color="gray"/>
-     <Sample color="black"/>
-     <Sample color="purple"/>
-     <Sample color="orange"/>
-     <Sample color="red"/>
-     <Sample color="blue"/>
-     <Sample color="gray"/>
-     <Sample color="black"/>
-     <Sample color="purple"/>
-     <Sample color="orange"/>
+      <MechanicDash />
     </div>
   );
 }
