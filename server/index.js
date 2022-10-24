54b8436d451123774e04f5302e510e701a23a754
BhanukaRajk <bhanukayar@gmail.com>
2022-10-24 17:39:45 +0530

    UPDATED INDEX.JS FILE IN SERVER

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 const username = process.env.DB_USERNAME || "admin";
-const passwd = process.env.DB_PASSWORD || "1234567890";
+const password = process.env.DB_PASSWORD || "1234567890";
 
 
 const app = Express();
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/mechanic',mechanic);
 app.use('/repair', repair);
-const CONNECTION_url =`mongodb+srv://${username}:${passwd}@cluster0.tfcjplj.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECTION_url =`mongodb+srv://${username}:${password}@cluster0.tfcjplj.mongodb.net/?retryWrites=true&w=majority`;
 
 
 mongoose.connect (CONNECTION_url, {
