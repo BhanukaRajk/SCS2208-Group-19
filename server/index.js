703e67baba2b41024b9c052312328438becd6470
kavishka-sulakshana <kss20001218@gmail.com>
2022-10-24 14:30:01 +0530

    updated index js

--- server/index.js
+++ server/index.js
@@ -3,25 +3,31 @@ import Express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
+import admin from "./routes/adminRouter.js";
 
 dotenv.config();
 const username = process.env.DB_USERNAME || "admin";
 const passwd = process.env.DB_PASSWORD || "1234567890";
 
-
 const app = Express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/mechanic',mechanic);
-const CONNECTION_url =`mongodb+srv://${username}:${passwd}@cluster0.tfcjplj.mongodb.net/?retryWrites=true&w=majority`;
 
+app.use("/mechanic", mechanic);
+app.use("/admin", admin);
 
-mongoose.connect (CONNECTION_url, {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
-.then (() => console.log("connection is established and running"))
-.catch((err) => console.log(err.message));
+const CONNECTION_url = `mongodb+srv://${username}:${passwd}@cluster0.tfcjplj.mongodb.net/?retryWrites=true&w=majority`;
 
-app.listen( process.env.PORT || 3001, () => console.log("server started on port 3001"));
+mongoose
+    .connect(CONNECTION_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("connection is established and running"))
+    .catch((err) => console.log(err.message));
+
+app.listen(process.env.PORT || 3001, () =>
+    console.log("server started on port 3001")
+);
