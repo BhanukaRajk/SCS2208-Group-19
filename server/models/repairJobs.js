ce676f940fe7e708776f76f022e99e3155001c3b
BhanukaRajk <bhanukayar@gmail.com>
2022-10-24 17:40:24 +0530

    commented model file

--- server/models/repairJobs.js
+++ server/models/repairJobs.js
@@ -1,22 +1,35 @@
 import mongoose from "mongoose";
 
+// SCHEMA
 const repairSchema = mongoose.Schema({
+
+    // REQUESTING CLIENT'S NAME
     client_name : {
+        // DATA TYPE IS STRING AND CANNOT BE NULL
         type : String,
         require : true
     },
+
+    // VEHICLE'S CURRENT LOCATION
     location : {
+        // DATA TYPE IS STRING AND CANNOT BE NULL
         type : String,
         require : true
     },
+
+    // MOBILE NUMBER OF THE CLIENT
     client_mobile : String,
+
+    // VEHICLE MODEL OF THE CLIENT
     vehicle_model : String,
+
+    // RECORD ACCEPTANCE BY A MECHANIC
     acceptance : {
+        // MECHANIC'S NAME MOBILE NUMBER AND ACCCEPTED TIME
         acceptedby : String,
         mechanic_mobile : String,
         added_date : Date
     }
-
 
 });
 
