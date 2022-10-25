aa1e3c99ce11c254f17fcb2ac94640c124edcfe2
Binula Rasanjith <binula28@gmail.com>
2022-10-25 07:02:32 +0530

    minor changes

--- client/src/components/admin/ClientDashboard.jsx
+++ client/src/components/admin/ClientDashboard.jsx
@@ -1,8 +1,13 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const ClientDashboard = () => {
-	const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
+    
+    const [name, setName] = useState([]);
+    const [email, setEmail] = useState([]);
+    const [mobileNo, setMobileNo] = useState([]);
+    const [vehicles, setVehicles] = useState([]);
 
 	const getData = () => {
 		axios
@@ -13,12 +18,63 @@ const ClientDashboard = () => {
 			.catch((error) => {
 				consoloe.log(`Error: ${error}`);
 			});
-    };
-    
-    useEffect(() => { getData(); }, []);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
 
 	return (
 		<div className="container w-50">
+			<form>
+				<div className="mb-3">
+					<label className="form-label">Name</label>
+					<input
+						type="text"
+						className="form-control"
+						onChange={(e) => {
+							setName(e.target.value);
+						}}
+						value={name}
+					/>
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Email address</label>
+					<input
+						type="email"
+						className="form-control"
+						onChange={(e) => {
+							setEmail(e.target.value);
+						}}
+						value={email}
+					/>
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Mobile Number</label>
+					<input
+						type="text"
+						className="form-control"
+						onChange={(e) => {
+							setMobileNo(e.target.value);
+						}}
+						value={mobileNo}
+					/>
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Vehicles</label>
+					<input
+						type="text"
+						className="form-control"
+						onChange={(e) => {
+							setVehicles(e.target.value);
+						}}
+						value={vehicles}
+					/>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
 			<table className="table">
 				<thead>
 					<tr>
